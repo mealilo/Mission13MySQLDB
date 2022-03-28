f4fe1bf1a4a5164777c8549bcc0571ab4eda2a4d
mealilo <90289295+mealilo@users.noreply.github.com>
2022-03-28 14:48:05 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission13MySQLDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
